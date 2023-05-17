ca02582c657c6ec15a55887d3be11f35156c7045
Jan Cordeiro <121439832+jancordeiro@users.noreply.github.com>
2023-05-17 11:59:26 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -76,3 +76,31 @@ function restrict_dashboard_access() {
 add_action('template_redirect', 'restrict_dashboard_access');
 
 // FIM DO SCRIPT DA VERIFICAÇÃO DE LOGIN
+
+/*
+ * 
+ * CRIAR BOTÃO DE LOGOUT
+ */
+
+function generate_logout_button() {
+    // Verifica se o usuário está logado
+    if (is_user_logged_in()) {
+        // Obtém o link da página inicial
+        $home_url = home_url('/');
+
+        // Cria o botão de logout redirecionando para a página inicial
+        $logout_url = wp_logout_url($home_url);
+
+        // Retorna o HTML do botão de logout
+        return '<div class="logout-button"><a class="logout-btn" href="' . $logout_url . '"><i aria-hidden="true" class="fas fa-sign-out-alt"></i> Logout (Sair)</a></div>';
+    }
+}
+
+// CRIA O SHORTCODE PARA INVOCAR O BOTÃO
+
+function logout_button_shortcode() {
+    return generate_logout_button();
+}
+add_shortcode('logout_button', 'logout_button_shortcode');
+
+// FIM DO BOTÃO E SHORTCODE SAIR/LOGOUT
